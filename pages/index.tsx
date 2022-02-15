8206c94a9c19e26f7bb9a5969d68252b22467f47
Liedel <liedel75@gmail.com>
2022-02-15 22:11:49 +0100

    add post section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import Header from '../components/Header'
 import { sanityClient, urlFor } from '../sanity'
 import { Post } from '../typings'
@@ -38,7 +39,32 @@ export default function Home({posts}: Props) {
           alt="M" 
         />
       </div>
-
+    
+    {/* Posts */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+        {posts.map((post)=> (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className='border rounded-lg group cursor-pointer'>
+              <img 
+                className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out'
+                src={urlFor(post.mainImage).url()!} 
+                alt="post1" 
+              />
+              <div className='flex justify-between p-5 bg-white'>
+                 <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p>{post.description} by {post.author.name}</p>
+                </div>
+                <img
+                  className='h-12 w-12 rounded-full' 
+                  src={urlFor(post.author.image).url()!} 
+                  alt="picture" 
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
